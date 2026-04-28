a5c7116632e08bfca4ebc5b9cbbe302d5c7ab5f6
UltralyticsAssistant <web@ultralytics.com>
2026-04-28 04:59:12 +0000

    Auto-format by https://ultralytics.com/actions

--- predict_my.py
+++ predict_my.py
@@ -1,13 +1,12 @@
 from ultralytics import YOLO
 
-model = YOLO('weights/best.pt')
+model = YOLO("weights/best.pt")
 model.predict(
-   source=0,
+    source=0,
     imgsz=640,
     conf=0.5,
     iou=0.5,
     device=0,
     show=True,
     save=False,
-
 )
